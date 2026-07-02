c38442e26ad19ea7ed545d1e9c8623061fd5e9cc
Yvonnou Théo <tyvonnou@gmail.com>
2026-07-02 12:00:20 +0200

    FEAT: Init

---
+++ README.md
@@ -0,0 +1,19 @@
+# Tools
+
+## Installation 
+
+1- Cloner le projet sur votre poste.
+
+```bash
+git clone --recurse-submodule <repo-URL>
+```
+
+2- Ajouter dans `~/.bashrc`, en modifiant le chemin si nécéssaire : 
+
+```bash linenums="1" 
+# Fonctions
+if [ -f "${HOME}/Documents/git/tools/bash/home.sh" ]; then
+   source "${HOME}/Documents/git/tools/bash/home.sh"
+fi
+```
+
---
+++ bash/home.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+declare -r BASH_PATH="$(dirname "$(realpath "${BASH_SOURCE}")")"
+declare -r PARENT_PATH="$(dirname "$(dirname "$(realpath "${BASH_SOURCE}")")")"
+# imports
+# source "${BASH_PATH}/git.sh"
+
+
+# Prompt GIT
+if [ -f "${PARENT_PATH}/lib/bash-git-prompt/gitprompt.sh" ]; then
+    GIT_PROMPT_ONLY_IN_REPO=1
+    source "${PARENT_PATH}/lib/bash-git-prompt/gitprompt.sh"
+fi
+
+# alias
+if exist codium; then
+  alias c.="codium ."
+elif exist code; then
+  alias c.="code ."
+fi
+
+alias e.="xdg-open ."
+
+alias gcm_log="git log --graph --decorate --pretty=oneline --abbrev-commit --all"
+alias gcm_log_all="git log --graph --pretty=tformat:'%C(auto)%d%Creset - %Cred%h%Creset - %Cblue(%an %ar)%Creset%n%s%n%b' --all"
+
